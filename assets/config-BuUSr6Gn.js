7c84d086315fafa55963e09140b759bb9ca5be89
JonSnow1807 <JonSnow1807@users.noreply.github.com>
2025-09-11 02:36:46 +0000

    deploy: 4343fee50096f10187508c4e4b3d07b616886687

---
+++ assets/config-BuUSr6Gn.js
@@ -0,0 +1,2 @@
+const t=`ws://${window.location.host}`,s=()=>`${window.location.protocol==="https:"?"wss:":"ws:"}//${window.location.host}/ws`,n=o=>o.startsWith("/")?`${o}`:`/${o}`,c={API_BASE_URL:"",WS_BASE_URL:t,getWebSocketURL:s,getApiUrl:n,isDevelopment:!1,isProduction:!0};export{s as a,c,n as g};
+//# sourceMappingURL=config-BuUSr6Gn.js.map
